cb7908fd47821c11bf0d29be27a0e132c05c1cde
Tim Jarratt <tjarratt@gmail.com>
2015-05-27 22:19:02 -0700

    First commit

---
+++ MinesOfMios/Specs/MinesOfMiosTests.swift
@@ -0,0 +1,15 @@
+import UIKit
+import XCTest
+import Quick
+import Nimble
+import MinesOfMios
+
+class MinessOfMiosSpec: QuickSpec {
+    override func spec() {
+        describe("a spec") {
+            it("should have been wrote") {
+                expect(true).to(beFalsy())
+            }
+        }
+    }
+}
